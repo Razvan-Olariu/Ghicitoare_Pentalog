09bc4647a973b4205c483c323017a59038175edb
Alexandru-Razvan Olariu <ufcolonel@gmail.com>
2019-12-12 13:45:42 +0200

    Am adaugat ambele proiecte, respectiv folderele lor.
    Ambele proiecte pot fi vizualizate deschizand folderul lor specific.

---
+++ Formatare String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formatare_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nume1, nume2;
+            nume1 = "Olariu Alexandru-Razvan";
+            nume2 = "Ovidiu Radoi";
+            Console.WriteLine(string.IsNullOrEmpty(nume1)); // 1
+            Console.WriteLine(string.Compare(nume1, nume2)); // 2
+            Console.WriteLine(String.Join(" ", nume1, nume2)); // 3
+            Console.WriteLine(nume1.ToUpper()); // 4
+            Console.WriteLine(nume2.ToLower()); // 5
+            Console.WriteLine(nume1.StartsWith("O")); //6
+            Console.WriteLine(nume2.EndsWith("Radoi")); // 7
+
+
+        }
+    }
+}
---
+++ Pentalog_Tema2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentalog_Tema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year, month, day;
+            string gender;
+
+            Console.WriteLine("Te rog sa precizezi anul in care te-ai nascut:");
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Te rog sa precizezi luna in care te-ai nascut:");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Te rog sa precizezi ziua in care te-ai nascut:");
+            day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Te rog sa precizezi cu M/F sexul tau:\n\t(Cu litera mare!!)");
+            gender = Convert.ToString(Console.ReadLine());
+            DateTime value = new DateTime(year, month, day);
+            Console.WriteLine("Te-ai nascut in data de:\n\t");
+            Console.WriteLine(value);
+            DateTime now = DateTime.UtcNow;
+            TimeSpan diff = now - value;
+            string diferenta = String.Format("{0:0.#}", diff.TotalDays);
+            Console.WriteLine($"\n{diferenta} zile ai trait de cand te-ai nascut.\n");
+            Console.WriteLine("\n\tDupa punct, sunt reprezentate orele\n\n\n");
+            double pensie = Convert.ToDouble(diferenta);
+            if (gender == "M" && pensie / 365 > 65)
+                Console.WriteLine("Esti un pensionar ! ;) ");
+            else if (gender == "F" && pensie / 365 > 63)
+                Console.WriteLine("Esti o pensionara ! ;) "); 
+            else if ((gender != "M" || gender != "F") && (pensie / 365 > 63 || pensie / 365 > 65))
+                Console.WriteLine("Esti pensionar. ( Nu am specificat genul)");
+            else
+                Console.WriteLine("Esti un om muncitor!! :D");
+                    
+         }
+    }
+}
